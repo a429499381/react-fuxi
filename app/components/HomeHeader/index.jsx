6e6b9339035a9b37bdc9ad134ac47179d286b172
xutao <429499381@qq.com>
2017-07-24 08:34:04 +0800

    开始React 第一步，黑苹果还是容易出问题， 二手硬件质量， 兼容性，折腾死人， 期待能有垃圾桶，为效率，高兴充值。

--- app/components/HomeHeader/index.jsx
+++ app/components/HomeHeader/index.jsx
@@ -20,7 +20,7 @@ class HomeHeader extends React.Component {
                 </Link>
                 <div className="search">
                     <input type="text" placeholder="请输入要搜索内容"/>
-                    <i className="search_icon"></i>
+                    <i className="search_icon"> </i>
                 </div>
                 <a href="#" className="user">用户</a>
             </div>
