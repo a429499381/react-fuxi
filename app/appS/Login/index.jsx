0a28a522b365a8712e920be45ee30c331e071b36
xutao <429499381@qq.com>
2017-07-26 16:34:27 +0800

    填写完了 用户名后无法正确跳转到 用户界面 ， 大致原因无法正确读取  userinfo.username

--- app/appS/Login/index.jsx
+++ app/appS/Login/index.jsx
@@ -15,6 +15,9 @@ class Login extends React.Component {
     constructor(props, context) {
         super(props, context);
         this.shouldComponentUpdate = PureRenderMixin.shouldComponentUpdate.bind(this);
+        this.state = {
+            username: ''
+        }
     }
     render() {
         return (
@@ -23,16 +26,34 @@ class Login extends React.Component {
                 <h1>登陆</h1>
                 <div className="user_login">
                     <span className="user_name">用户名</span>
-                    <input type="text" className="userName"/>
+                    <input type="text" className="userName" onChange={this.changeUser.bind(this)}/>
+                    <span>{this.state.username}</span>
                     <span className="user_pass">密码</span>
                     <input type="password" className="userPass"/>
-                    <a href="javascript:;" className="login_btn">登陆</a>
+                    <a href="javascript:;" className="login_btn" onClick={this.login.bind(this)}>登陆</a>
                 </div>
             </div>
         )
     }
 
+    changeUser(e) {
+        this.setState({
+            username: e.target.value
+        })
+        let userinfo = this.props.userinfo
+        userinfo.username = username
+        this.props.userInfoActions.update(userinfo)
+    }
 
+    login() {
+        if (this.state.username) {
+            hashHistory.push('/User')
+            console.log(this.state.username)
+        } else {
+            hashHistory.push('/Login')
+            console.log("go Login")
+        }
+    }
 
 }
 
