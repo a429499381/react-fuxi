bdc10f08fa831b3bf8f345b0d550fb74f1d7f3ab
xutao <429499381@qq.com>
2017-07-26 08:50:16 +0800

    写城市选择界面  无法从首页跳转  ok

--- app/appS/City/index.jsx
+++ app/appS/City/index.jsx
@@ -2,6 +2,12 @@ import React from 'react'
 import PureRenderMixin from 'react-addons-pure-render-mixin'
 import { bindActionCreators } from 'redux'
 import { connect } from 'react-redux'
+import { hashHistory } from 'react-router'
+
+import * as userInfoActionsFromOtherFile from '../../actions/userinfo'
+
+import { CITYNAME } from '../../config/localStoreKey'
+import localStore from '../../util/localStore'
 
 import './sub/style.less'
 
@@ -13,14 +19,29 @@ class City extends React.Component {
     render() {
         return (
             <div>
-                <div className="list">
-                    <a href="#" onClick={this.ClickHandle.bind(this, '武汉')}>武汉</a>
-                    <a href="#" onClick={this.ClickHandle.bind(this, '上海')}>上海</a>
-                    <a href="#" onClick={this.ClickHandle.bind(this, '天津')}>天津</a>
-                    <a href="#" onClick={this.ClickHandle.bind(this, '北京')}>北京</a>
+                <div className="click_cityName">
+                    <a href="#" onClick={this.changeCity.bind(this, '武汉')}>武汉</a>
+                    <a href="#" onClick={this.changeCity.bind(this, '上海')}>上海</a>
+                    <a href="#" onClick={this.changeCity.bind(this, '天津')}>天津</a>
+                    <a href="#" onClick={this.changeCity.bind(this, '北京')}>北京</a>
                 </div>
             </div>
         )
+    }
+    changeCity(newCity) {
+        if (newCity == null) {
+            return
+        }
+        // 修改 redux
+        const userinfo = this.props.userinfo
+        userinfo.cityName = newCity
+        this.props.userInfoActions.update(userinfo)
+
+        // 修改 cookie
+        localStore.setItem(CITYNAME, newCity)
+
+        // 跳转页面
+        hashHistory.push('/')
     }
 }
 
@@ -34,6 +55,7 @@ function mapStateToProps(state) {
 
 function mapDispatchToProps(dispatch) {
     return {
+        userInfoActions: bindActionCreators(userInfoActionsFromOtherFile, dispatch)
     }
 }
 export default connect(
