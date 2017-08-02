3f40a3920bbb9239ef2cb290c8ba9fcaf8250cd4
xutao <429499381@qq.com>
2017-08-03 00:20:08 +0800

    用户 评价 开始

--- app/appS/User/index.jsx
+++ app/appS/User/index.jsx
@@ -3,7 +3,7 @@ import PureRenderMixin from 'react-addons-pure-render-mixin'
 import { bindActionCreators } from 'redux'
 import { connect } from 'react-redux'
 import { hashHistory } from 'react-router'
-
+import { getOrderListData, postComment } from '../../fetch/user/orderlist'
 import * as userInfoActionsFromOtherFile from '../../actions/userinfo'
 
 import { CITYNAME } from '../../config/localStoreKey'
@@ -15,11 +15,17 @@ class User extends React.Component {
     constructor(props, context) {
         super(props, context);
         this.shouldComponentUpdate = PureRenderMixin.shouldComponentUpdate.bind(this);
+        this.state = {
+          OrderData: [],
+          Comment: [],
+          commentState: '',
+        }
     }
     render() {
+        const data = this.state.OrderData
         return (
             <div>
-                <h1>用户界面</h1>
+                <h2>用户界面</h2>
                 <p className="cityname">
                     {this.props.userinfo.cityName}
                 </p>
@@ -27,11 +33,37 @@ class User extends React.Component {
                     {this.props.userinfo.username}
                 </p>
 
+                <div className="OrderData">
+                  {
+                      data.map((item, index) => {
+                          return (
+                            <div className="item" key={index} id={item.id}>
+                                <p>{item.title}</p>
+                                <img src={item.img}/>
+                                <p>{item.price}</p>
+                                <div>
+                                  {
+                                      this.state.commentState === 0
+                                        ? <a href="javascript:;">评价</a>
+                                        :   this.state.commentState === 2
+                                          ? <a href="javascript:;">已评价</a>
+                                          : ''
+                                  }
+
+                                </div>
+                            </div>
+                          )
+                      })
+                  }
+                </div>
+
+
+
             </div>
         )
     }
     componentDidMount() {
-
+        const that = this
         let username = localStore.getItem('username')
         let cityname = localStore.getItem('CITYNAME')
         let userinfo = this.props.userinfo
@@ -40,12 +72,21 @@ class User extends React.Component {
 
 
 
+
         if (!this.props.userinfo.username) {
             hashHistory.push('/Login')
         }
 
-
-
+        const result = getOrderListData(username)
+        result.then(res => {
+            return res.json()
+        }).then(json => {
+            const data = json
+            that.setState({
+              OrderData: data,
+            })
+           console.log(data)
+        })
     }
 
 }
