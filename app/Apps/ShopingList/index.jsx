ebf20bb4b225519986adf1878f9176afd390c403
xutao <429499381@qq.com>
2017-08-05 22:11:20 +0800

    用户 购买商品数据 刷新后报错， 提示数据没有定义。

--- app/Apps/ShopingList/index.jsx
+++ app/Apps/ShopingList/index.jsx
@@ -7,25 +7,60 @@ import * as userInfoActionsFromOtherFile from '../../actions/userinfo'
 import { hashHistory } from 'react-router'
 import './style.less'
 import ShopingList from '../ShopingList'
+import { getOrderListData } from '../../fetch/user/orderlist'
 
 class User extends React.Component {
     constructor(props) {
         super(props)
+        this.state ={
+            data: '',
+        }
     }
 
     // 渲染页面
     render() {
+        const data = this.state.data
         return (
-            <div className="ShopingList">
-                ShopingList
+           <div className="ShopingList">
+              {
+                  data.map((item, index) => {
+                      return (
+                        <div className="item" key ={index} id = {item.id}>
+                            <h3>{item.title}</h3>
+                            <img src={item.img}></img>
+                            <p>{item.commentState}</p>
+                            <p>{item.count}</p>
+                            <p className="price">单价:{item.price}</p>
+                        </div>
+                      )
+                  })
+              }
             </div>
+
+
+
         )
     }
 
     // 生命周期函数
     componentDidMount() {
+      const username = this.props.userinfo.username
+      console.log(username)
+      if (username) {
+          this.GetData()
+      }
+    }
 
-
+    GetData() {
+      const result = getOrderListData()
+      result.then(res => {
+        return res.json()
+      }).then(json => {
+        this.setState({
+          data: json
+        })
+        console.log(json)
+      })
     }
 
 }
