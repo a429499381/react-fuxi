5f3f78a1ffe2d6e3c92968c9075e579a571c0f4a
xutao <429499381@qq.com>
2017-08-06 12:53:23 +0800

    详情页数据请求   对于数据中有html语法  正确解析方法    <p dangerouslySetInnerHTMl ={{__html: data.desc}}></p>

--- app/Apps/Detail/subCom/Info/index.jsx
+++ app/Apps/Detail/subCom/Info/index.jsx
@@ -1,37 +1,52 @@
 import React from 'react'
-import PureRenderMixin from 'react-addons-pure-render-mixin'
-
 
 import './style.less'
 
 class Info extends React.Component {
     constructor(props, context) {
-        super(props, context);
-        this.shouldComponentUpdate = PureRenderMixin.shouldComponentUpdate.bind(this);
+        super(props, context)
     }
-    render() {
-        // 获取数据
-        const data = this.props.data
 
+    // 渲染页面
+    render() {
+        const data = this.props.data
         return (
-            <div id="detail-info-container">
-                <div className="info-container clear-fix">
-                    <div className="info-img-container float-left">
-                        <img src={data.img} />
-                    </div>
-                    <div className="info-content">
-                        <h1>{data.title}{data.length}</h1>
-                        <div className="star-container">
-                            {/* 引用 Star 组件 */}
-                            <span className="price">￥{data.price}</span>
-                        </div>
-                        <p className="sub-title">{data.subTitle}</p>
-                    </div>
+            <div>
+                <div className="Hearder">
+                    <a href="#">返回</a>
+                    <h2>商户详情</h2>
                 </div>
-                {/* 设置 innerHTML */}
-                <p dangerouslySetInnerHTML={{__html: data.desc}} className="info-desc"></p>
+                <div className="top">
+                    <img src="" alt=""/>
+                    <p className="title">{data.title}</p>
+                    <div className="com">
+                        <span className="start">5</span>
+                        <p dangerouslySetInnerHTML={{__html: data.desc}}></p>
+                        <span className="pl">288条</span>
+                        <span className="price">88/</span>
+                    </div>
+                    <p className="name">双井 重庆火锅</p>
+                    <p className="pf">
+                        <span>口味:9.0</span>
+                        <span>环境：8.7</span>
+                        <span>服务：8.9</span>
+                    </p>
+                    <p className="address">
+                        <span className="txt">双井北街与南街交汇出</span>
+                        <a href="javascript:;">></a>
+                    </p>
+                    <p className="phone">
+                        <span className="numb">40121123456</span>
+                        <a href="javascript:;">></a>
+                    </p>
+                </div>
             </div>
         )
+    }
+
+    // 生命周期函数
+    componentDidMount() {
+
     }
 }
 
